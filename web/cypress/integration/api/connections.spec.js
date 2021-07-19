479ec0516a9d47bfb9dc7d5c342477db382094df
camillamsantos <camillamsantos.sm@gmail.com>
2021-07-18 23:07:09 -0300

    Update connections.spec.js

--- web/cypress/integration/api/connections.spec.js
+++ web/cypress/integration/api/connections.spec.js
@@ -7,20 +7,20 @@ context('Connections endpoint', () => {
         cy.api({
             method: 'GET',
             url: `${Cypress.config().apiUrl}/connections`
-        }).then((response) => {
-            expect(response.status).to.eq(200)
-            expect(response.duration).lessThan(10)
+        }).then((resConnections) => {
+            expect(resConnections.status).to.eq(200)
+            expect(resConnections.duration).lessThan(10)
 
-            expect(response.body)
+            expect(resConnections.body)
                 .to.have.property('total')
                 .to.be.a('number')
                 .greaterThan(5)
 
-            expect(response.body.total)
+            expect(resConnections.body.total)
                 .an('number')
                 .satisfy((totalValue) => { return totalValue >= 5 })
 
-            expect(response.headers)
+            expect(resConnections.headers)
                 .to.have.property('content-type')
                 .an('string')
                 .equal('application/json; charset=utf-8')
